27bfcf191fe21f33629e260e8620a469cb595be3
Edward Heath <e42291+github@gmail.com>
2018-12-07 12:44:59 -0800

    Complete 02

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,37 @@
 #write your code here
+
+def add a, b
+	a + b
+end
+
+def subtract a, b
+	a - b
+end
+
+def sum arr
+	total = 0
+	arr.each do |i|
+		total += i
+	end
+	return total
+end
+
+def multiply *args
+	product = 1
+	args.each do |i|
+		product *= i
+	end
+	return product
+end
+
+def power a, b
+	a ** b
+end
+
+def factorial a
+	if a == 0
+		return 1
+	else
+		return factorial(a-1) * a
+	end		
+end
