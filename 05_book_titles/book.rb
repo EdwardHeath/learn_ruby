7cf9ba1537cee65cb99649874084b25e3d87d95d
Edward Heath <e42291+github@gmail.com>
2018-12-09 18:01:01 -0800

    Complete 05

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,27 @@
 class Book
 # write your code here
+	attr_reader :title
+
+	def initialize
+		@title = ""
+	end
+
+	def title=(title)
+		title = title.split(' ')
+		new_title = []
+		avoid = ['and', 'in', 'the', 'of', 'a', 'an']
+
+		title.each do |word|
+			if !avoid.include?(word)
+				new_title.push(word.capitalize())
+			else
+				new_title.push(word)
+			end
+		end
+
+		new_title[0] = new_title[0].capitalize()
+		
+		@title = new_title.join(' ')
+	end
+	
 end
