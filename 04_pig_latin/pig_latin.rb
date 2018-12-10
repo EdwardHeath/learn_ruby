74dff7de25cde3103a7e2318df181b734318896d
Edward Heath <e42291+github@gmail.com>
2018-12-09 17:43:57 -0800

    Complete 04

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,24 @@
 #write your code here
+
+def translate words
+	vowels = ['a', 'e', 'i', 'o', 'u']
+	words = words.split(' ')
+	translated = []
+
+	words.each do |word|
+		moved = ""
+
+		for i in 0..(word.length - 1)
+			if word[i] == "q" && word[i+1] == "u"
+				translated.push(word[i+2..-1] + moved + "quay")
+				break
+			elsif !vowels.include?(word[i])
+				moved += word[i]
+			else
+				translated.push(word[i..-1] + moved + "ay")
+				break
+			end
+		end
+	end
+	return translated.join(' ')
+end
